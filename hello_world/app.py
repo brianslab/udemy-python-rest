fc323d96045cd5d8316a2c5325519d24f394ec5c
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-22 02:07:56 -0800

    json example

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,26 @@ def hello_world():
 @app.route('/hithere')
 def hi_there():
     return "Welcome to /hithere!"
+
+@app.route('/json')
+def json_example():
+    age = 2022 - 1996
+    retJson = {
+        'Name': 'Brian',
+        'Age': age,
+        'pets': [
+            {
+                'name': 'Greg',
+                'type': 'cat'
+            },
+            {
+                'name': 'Sofi',
+                'type': 'cat'
+            }
+
+        ]
+    }
+    return jsonify(retJson)
 
 if __name__=="__main__":
     app.run(debug=True)
