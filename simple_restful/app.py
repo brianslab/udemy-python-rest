f71d691c0d620b9c73822e2ab8509654be20f43b
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-22 04:42:05 -0800

    create add route

--- simple_restful/app.py
+++ simple_restful/app.py
@@ -1,7 +1,34 @@
 from flask import Flask, jsonify, request
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+class Add(Resource):
+    def post(self):
+        # resource Add was requested with POST
+        postedData = request.get_json()
+        x = int(postedData["x"])
+        y = int(postedData["y"])
+
+        result = x + y
+        resultJSON = {
+            'Status Code': 200,
+            'Message': result
+        }
+        return jsonify(resultJSON)
+
+
+class Subtract(Resource):
+    pass
+
+class Multiply(Resource):
+    pass
+
+class Divide(Resource):
+    pass
+
+api.add_resource(Add, "/add")
 
 @app.route('/')
 def hello_world():
