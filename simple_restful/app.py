199037f70818852f3c91ec7cf58cbbdd20ac60eb
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-22 23:29:08 -0800

    add subtract method

--- simple_restful/app.py
+++ simple_restful/app.py
@@ -4,12 +4,14 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
 def checkPostedData(postedData, function):
-    if (function == 'add'):
+    if (function in ['add', 'subtract']):
         if "x" not in postedData or "y" not in postedData:
-            return 301;
+            return 301
         else:
-            return 200;
+            return 200
+
 
 class Add(Resource):
     def post(self):
@@ -35,19 +37,44 @@ class Add(Resource):
 
 
 class Subtract(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        statusCode = checkPostedData(postedData, "subtract")
+
+        if (statusCode != 200):
+            resultJSON = {
+                'Message': 'An error has occured',
+                'Status Code': statusCode
+            }
+            return jsonify(resultJSON)
+
+        x = int(postedData["x"])
+        y = int(postedData["y"])
+
+        result = x - y
+        resultJSON = {
+            'Status Code': 200,
+            'Message': result
+        }
+        return jsonify(resultJSON)
+
 
 class Multiply(Resource):
     pass
 
+
 class Divide(Resource):
     pass
 
+
 api.add_resource(Add, "/add")
+api.add_resource(Subtract, "/subtract")
+
 
 @app.route('/')
 def hello_world():
     return "Hello from simple_restful!"
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
