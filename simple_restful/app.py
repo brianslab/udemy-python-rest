ff9c72f8f6a6d7c37fd748249ab67c1261865b5a
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-22 23:35:40 -0800

    add divide method

--- simple_restful/app.py
+++ simple_restful/app.py
@@ -11,6 +11,13 @@ def checkPostedData(postedData, function):
             return 301
         else:
             return 200
+    elif (function == 'divide'):
+        if "x" not in postedData or "y" not in postedData:
+            return 301
+        elif int(postedData["y"]) == 0:
+            return 302
+        else:
+            return 200
 
 
 class Add(Resource):
@@ -74,7 +81,7 @@ class Multiply(Resource):
         x = int(postedData["x"])
         y = int(postedData["y"])
 
-        result = x * y
+        result = x*y
         resultJSON = {
             'Status Code': 200,
             'Message': result
@@ -83,15 +90,35 @@ class Multiply(Resource):
 
 
 class Divide(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        statusCode = checkPostedData(postedData, "divide")
+
+        if (statusCode != 200):
+            resultJSON = {
+                'Message': 'An error has occured',
+                'Status Code': statusCode
+            }
+            return jsonify(resultJSON)
+
+        x = int(postedData["x"])
+        y = int(postedData["y"])
+
+        result = (x*1.0)/y
+        resultJSON = {
+            'Status Code': 200,
+            'Message': result
+        }
+        return jsonify(resultJSON)
 
 
 api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract")
 api.add_resource(Multiply, "/multiply")
+api.add_resource(Divide, "/divide")
 
 
-@app.route('/')
+@ app.route('/')
 def hello_world():
     return "Hello from simple_restful!"
 
